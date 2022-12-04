54dd2cb917f2882447aca02c56091226bdcbda66
xpwu <diodexp@foxmail.com>
2022-12-05 00:11:43 +0800

    add src

---
+++ index.ts
@@ -0,0 +1,10 @@
+
+export {Client, Result} from "./src/client"
+
+export {Utf8} from "./src/utf8"
+
+export {Event, ErrorEvent, WebSocketConstructor, WebSocketInterface, CloseEvent, Connection, MessageEvent} from "./src/connection"
+
+export {Option, RequestTimeout, ConnectTimeout, WebSocket} from "./src/option"
+
+export {ConnError} from "./src/connerror"
